ad6d26d0a428823bfb191e7a932adfb9a0783cb3
gideonrynn <sarah.c.dillard@gmail.com>
2020-03-26 16:36:36 -0500

    adding test files

---
+++ client/build/precache-manifest.fa83bf02ece7bbbfb6829679a8309904.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c91e32c0859f31e686f4",
+    "url": "/static/css/main.4e429ad5.chunk.css"
+  },
+  {
+    "revision": "c91e32c0859f31e686f4",
+    "url": "/static/js/main.657f64ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d375347a68ee444e9c21",
+    "url": "/static/js/2.fdf39872.chunk.js"
+  },
+  {
+    "revision": "c0945db92b326aaebf545a3ad77f63ce",
+    "url": "/index.html"
+  }
+];
